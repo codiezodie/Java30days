cf9f512882d544a92618dbdc8af7f22f93371f40
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-09 18:07:07 +0530

    day 1 of 30 days java questions.

---
+++ src/Day1.java
@@ -0,0 +1,8 @@
+public class Day1 {
+    public static void main(String[] args){
+        System.out.println("Hello I am Zainab Ahmad!!");
+        System.out.println("Let's learn Java for 30 days!");
+        System.out.print("My goal: To master Java and DSA.");
+
+    }
+}
